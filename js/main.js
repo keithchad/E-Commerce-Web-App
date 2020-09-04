c9db5b3927ff6482acde007d7f03d8784d29202f
keithchad <63049827+keithchad@users.noreply.github.com>
2020-09-04 13:02:59 +0300

    products details page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //Menu Toggle
-
 var MenuItems = document.getElementById("MenuItems");
 MenuItems.style.maxHeight = "0px";
 function menutoggle() {
@@ -8,5 +7,26 @@ function menutoggle() {
     }else {
         MenuItems.style.maxHeight = "0px";
     }
-
 }
+
+//Product Gallery
+
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function() {
+    ProductImg.src = SmallImg[0].src;
+}
+
+SmallImg[1].onclick = function() {
+    ProductImg.src = SmallImg[1].src;
+}
+
+SmallImg[2].onclick = function() {
+    ProductImg.src = SmallImg[2].src;
+}
+
+SmallImg[3].onclick = function() {
+    ProductImg.src = SmallImg[3].src;
+}
+
